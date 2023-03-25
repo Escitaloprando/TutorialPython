508446282e8d904e17e2e6b88b10ea3c10b298cb
Escitaloprando <l.antonio.conci@gmail.com>
2023-03-25 20:21:44 -0300

    Testing

--- Desafios/desafio09 - Tabuada.py
+++ Desafios/desafio09 - Tabuada.py
@@ -1,6 +1,7 @@
-#faça um progama que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+#faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
 n=int(input('Qual tabuada você quer saber?'))
 for o in range(1, 11):
     r=n*o
     #print(n, "x", o, "é igual à", r)
     print('{} X {} é igual à {}'.format(n, o, r))
+#Teste
