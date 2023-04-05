4963de2a5468706629c43d5f2722b5a79faaa10b
Escitaloprando <l.antonio.conci@gmail.com>
2023-04-05 12:53:02 -0300

    atualização

--- Desafios/desafio09 - Tabuada.py
+++ Desafios/desafio09 - Tabuada.py
@@ -1,7 +1,7 @@
 #faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
-n=int(input('Qual tabuada você quer saber?'))
-for o in range(1, 11):
-    r=n*o
+numero=int(input('Qual tabuada você quer saber?'))
+for operador in range(1, 11):
+    resultado=numero*operador
     #print(n, "x", o, "é igual à", r)
-    print('{} X {} é igual à {}'.format(n, o, r))
+    print('{} X {} é igual à {}'.format(numero, operador, resultado))
 #Teste
