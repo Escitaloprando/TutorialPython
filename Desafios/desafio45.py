7d8b843ad419b789ad1bc35b6ee30f8142ac2434
Escitaloprando <l.antonio.conci@gmail.com>
2023-04-05 11:03:16 -0300

    resolução

--- Desafios/desafio45.py
+++ Desafios/desafio45.py
@@ -1,14 +1,24 @@
-import random
 #Crie um programa que faça o computador jogar Pedra, Papel e Tesoura com você.
-jogador = input('Vamos jogar Pedra, Papel e Tesoura! \n [1] PEDRA \n [2] PAPEL \n [3] TESOURA \n Sua jogada: ')
-# pedra(1) > tesoura(3) > papel(2) > pedra(1)
-jogadas = ['1', '2', '3']
-computador = random.choice(jogadas)
+
+import random
+inputJogador = int(input('Vamos jogar Pedra, Papel e Tesoura! \n [0] PEDRA \n [1] PAPEL \n [2] TESOURA \n Sua jogada: '))
+# pedra(0) > tesoura(2) > papel(1) > pedra(0)
+jogadas = ['Pedra', 'Papel', 'Tesoura']
+computador = random.choice(jogadas) # Seleciona aleatoriamente um item na lista 'jogadas'
+jogador = jogadas[inputJogador]# Lê o input númerico do jogador e usa para selecionar o item correspondente na lista
 # Computador vence:
-if (computador == "1" and jogador == "3") or (computador == "3" and jogador == "2") or (computador == "2" and jogador == "1"):
+if computador == jogadas[0] and jogador == jogadas[2]:
+    print('ERA DAS MÁQUINAS! O Computador venceu. \n Computador: {} \n Jogador: {} \n Tente novamente'.format(computador, jogador))
+elif computador == jogadas[2] and jogador == jogadas[1]:
+    print('ERA DAS MÁQUINAS! O Computador venceu. \n Computador: {} \n Jogador: {} \n Tente novamente'.format(computador, jogador))
+elif computador == jogadas[1] and jogador == jogadas[0]:
     print('ERA DAS MÁQUINAS! O Computador venceu. \n Computador: {} \n Jogador: {} \n Tente novamente'.format(computador, jogador))
 # Jogador Vence:
-elif (computador == "3" and jogador == "1") or (computador == "2" and jogador == "3") or (computador == "1" and jogador == "2"):
+elif computador == jogadas[2] and jogador == jogadas[0]:
+    print('Viva a humanidade! Você venceu. \n Computador: {} \n Jogador: {} \n Tente novamente'.format(computador, jogador))
+elif computador == jogadas[1] and jogador == jogadas[2]:
+    print('Viva a humanidade! Você venceu. \n Computador: {} \n Jogador: {} \n Tente novamente'.format(computador, jogador))
+elif computador == jogadas[0] and jogador == jogadas[1]:
     print('Viva a humanidade! Você venceu. \n Computador: {} \n Jogador: {} \n Tente novamente'.format(computador, jogador))
 # Empate:
 else:
