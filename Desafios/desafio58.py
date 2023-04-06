1e61ec018ad7b4b6d8f4bf6142e94cb28cd58a81
Escitaloprando <l.antonio.conci@gmail.com>
2023-04-06 14:11:59 -0300

    resolução

--- Desafios/desafio58.py
+++ Desafios/desafio58.py
@@ -1,2 +1,9 @@
 # Melhore o jogo do Desafio28 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador 
 # vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+import random
+chute = []
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sorteado = random.choice(numeros)
+while chute != sorteado:
+    chute = int(input('Tente adivinhar em qual número de 0 a 10 estou pensando! '))
+print(f'Parabéns, você acertou! Pensei no número {sorteado}')
