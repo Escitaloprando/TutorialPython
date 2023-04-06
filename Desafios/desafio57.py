e7268a6e1ef3d69dbbbba32135266e6d79790e26
Escitaloprando <l.antonio.conci@gmail.com>
2023-04-06 13:38:26 -0300

    desafios

---
+++ Desafios/desafio57.py
@@ -0,0 +1,2 @@
+# Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores "M" ou "F". Caso esteja errado, 
+# faça uma digitação novamente até ter um valor correto.
---
+++ Desafios/desafio62.py
@@ -0,0 +1,2 @@
+# Melhore o desafio61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa
+# encerra quando ele disse que quer mostrar 0 termos.
