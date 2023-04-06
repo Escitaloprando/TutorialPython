56cd0ca6e41665faf70295e0fb5f8c59c9b06e16
Escitaloprando <l.antonio.conci@gmail.com>
2023-04-06 13:57:23 -0300

    resolução

--- Desafios/desafio57.py
+++ Desafios/desafio57.py
@@ -1,2 +1,6 @@
 # Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores "M" ou "F". Caso esteja errado, 
 # faça uma digitação novamente até ter um valor correto.
+sexo = "a"
+while sexo not in ["M", "F"]:
+    sexo = str(input('Qual é seu sexo? [F/M] ')).upper()
+print(f'Seu sexo é {sexo}')
