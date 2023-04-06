fdaae0e430a4c2ec8ddca214ebe7862c52c0e75e
Escitaloprando <l.antonio.conci@gmail.com>
2023-04-06 15:42:45 -0300

    resolução

--- Desafios/desafio64.py
+++ Desafios/desafio64.py
@@ -1,3 +1,11 @@
 # Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário
 # digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi
 # a soma entre eles(desconsidere o flag)
+soma = 0
+numero = 0
+entrada = 1
+while entrada != 999:
+    entrada = int(input(f'Digite o {entrada}° número inteiro! '))
+    if entrada != 999:
+        soma += entrada
+print(f'A soma dos numeros digitados é {soma}')
