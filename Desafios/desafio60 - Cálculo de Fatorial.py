e8223f17f7abd4da3f546775f2721ae3f2cb7821
Escitaloprando <l.antonio.conci@gmail.com>
2023-04-07 10:40:40 -0300

    resolução

--- Desafios/desafio60 - Cálculo de Fatorial.py
+++ Desafios/desafio60 - Cálculo de Fatorial.py
@@ -1,1 +1,10 @@
 #Faça um programa que leia um número qualquer e mostre seu fatorial.
+from math import factorial
+numero = int(input('Digite um número para saber seu fatorial: '))
+contador = numero
+print(f'O fatorial de {numero} é: ')
+while contador > 0:
+    print(f'{contador}', end='') # end='' --> faz com que o print abaixo seja feito na mesma linha
+    print(' X ' if contador > 1 else ' = ', end='')
+    contador -= 1
+print(f'{factorial(numero)}')
